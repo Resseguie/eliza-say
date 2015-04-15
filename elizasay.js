2a18e0a9f514644a7eb114bfe8a3040b11816036
David Resseguie <david@resseguie.com>
2015-04-15 11:34:35 -0400

    Cleanup recursive call, print chat, more generic init

--- elizasay.js
+++ elizasay.js
@@ -1,21 +1,29 @@
 var ElizaBot = require("elizabot");
 var say = require("say");
 
-var alex = {};
-var victoria = {};
+var i = 0;
 
-alex.eliza = new ElizaBot();
-victoria.eliza = new ElizaBot();
+var chatbots = [{
+  "name": "Alex",
+  "voice": "Alex",
+  "bot": new ElizaBot()
+},{
+  "name": "Victoria",
+  "voice": "Victoria",
+  "bot": new ElizaBot()
+}];
 
-var initial = alex.eliza.getInitial();
-chat(initial);
-
-function chat(statement) {
-  say.speak('Alex', statement, function() {
-    var reply = victoria.eliza.transform(statement);
-    say.speak('Victoria', reply, function() {
-      var newStatement = alex.eliza.transform(reply);
-      chat(alex.eliza.transform(newStatement));
-    });
+var chat = function (chatbot, statement) {
+  console.log(chatbot.name + ": ", statement);
+  say.speak(chatbot.voice || chatbot.name, statement, function() {
+    i = (i === chatbots.length-1) ? 0 : i+1;
+    var nextBot = chatbots[i];
+    var reply = nextBot.bot.transform(statement);
+    chat(nextBot, reply);
   });
 }
+
+chat(
+  chatbots[i],
+  "Hi there, " + chatbots[i+1].name + ". How are you today?"
+);
